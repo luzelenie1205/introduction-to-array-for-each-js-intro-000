547dfcb2e35acc69450dc2b5966cb6dce14f9cd6
bright-generator-f905 <luzelenie1205@gmail.com>
2021-05-31 04:38:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
 // Add your doToElementsInArray() function here:
  function doToElementsInArray(){
-retrun 
- }
- function fruit(element,array)
- {
-   return `Mmmm, ${fruit}!!!`;
+
+
  }
 
 // Add your changeCompletely() function here:
