e3016f0c27ef6921b9e50c975d202ec5fd43065f
bright-generator-f905 <luzelenie1205@gmail.com>
2021-05-31 04:45:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 // Add your doToElementsInArray() function here:
- function doToElementsInArray(array,callback){
-
-array.foreach(callback);
-
+function doToElementsInArray(array, callback) {
+ array.forEach(callback);
 }
 // Add your changeCompletely() function here:
 function changeCompletely(element, index, array) {
